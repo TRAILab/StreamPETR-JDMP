c15f270b42ce731336367e9609fa6afc336dcd1a
sandropapais <papais.sandro@gmail.com>
2024-07-01 13:00:40 -0400

    dump yaml config for wandb logging

--- mmdetection3d/tools/train.py
+++ mmdetection3d/tools/train.py
@@ -177,6 +177,7 @@ def main():
     mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
     # dump config
     cfg.dump(osp.join(cfg.work_dir, osp.basename(args.config)))
+    cfg.dump(osp.join(cfg.work_dir, osp.basename(args.config))[:-2]+'yaml')
     # init the logger before other steps
     timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
     log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
