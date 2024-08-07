6756d1e40fcba84e8fa45037c211a52078ef03a4
Sandro Papais <papais.sandro@gmail.com>
2024-08-07 00:38:18 -0400

    modified cc run scripts for use with narval

--- tools/cc_test.sh
+++ tools/cc_test.sh
@@ -1,47 +1,35 @@
 #!/bin/bash
-#SBATCH --job-name=test_stream_petr_r50_flash_704_bs4_seq_428q_nui_60e_4gpu    # Job name
+#SBATCH --job-name=test_jdmp_baseline_bs4_4gpu    # Job name
 #SBATCH --account=rrg-swasland
 #SBATCH --ntasks=1                    # Run on n CPUs
-#SBATCH --mem=180gb                     # Job memory request
-#SBATCH --time=11:59:00               # Time limit hrs:min:sec
+#SBATCH --mem=120gb                     # Job memory request
+#SBATCH --time=2-23:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/home/spapais/output/streampetr_jdmp/%x-%j.log   # Standard output and error log
-#SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:t4:4
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:t4:4           # gpu:t4:4 (graham) or gpu:a100:1 (narval)
 #SBATCH --mail-user="sandro.papais@robotics.utias.utoronto.ca"
 #SBATCH --mail-type=ALL
 
 # Parameters
+SERVER=graham
 NUM_GPUS=4
-CFG_NAME=stream_petr_r50_flash_704_bs4_seq_428q_nui_60e_4gpu
-SING_IMG=/home/spapais/projects/rrg-swasland/singularity/streampetr.sif
-WANDB_MODE='offline'
-echo "SLURM_JOB_ID=$SLURM_JOB_ID
-CFG_NAME=$CFG_NAME
-NUM_GPUS=$NUM_GPUS
-"
+CFG_NAME=jdmp_baseline_bs4_4gpu
 
 # Host paths
-DATA_DIR=/home/spapais/projects/rrg-swasland/Datasets/nuscenes
 HOME_DIR=/home/spapais
 TMP_DATA_DIR=$SLURM_TMPDIR/data
 # TMP_DATA_DIR=/home/spapais/scratch/temp_data # Slurm unzip alternative
 PROJ_DIR=$HOME_DIR/StreamPETR-JDMP
 OUT_DIR=$HOME_DIR/output/streampetr_jdmp
-
-# Extract Dataset
-echo "Extracting data"
-SECONDS=0
-for file in $DATA_DIR/*.zip; do
-    duration=$SECONDS
-    echo "[$((duration/3600))h$((duration%3600/60))m]: Unzipping $file to $TMP_DATA_DIR"
-    unzip -qq $file -d $TMP_DATA_DIR
-done
-for file in $DATA_DIR/*.pkl; do
-    duration=$SECONDS
-    echo "[$((duration/3600))h$(((duration%3600)/60))m]: Copying $file to $TMP_DATA_DIR"
-    cp $file $TMP_DATA_DIR
-done
-echo "Done extracting data"
+SING_IMG=/home/spapais/projects/rrg-swasland/singularity/streampetr.sif
+if [ "$SERVER" = "graham" ]; then
+    DATA_DIR=/home/spapais/projects/rrg-swasland/Datasets/nuscenes
+    DATA_PKL_DIR=/home/spapais/projects/rrg-swasland/Datasets/nuscenes
+fi
+if [ "$SERVER" = "narval" ]; then
+    DATA_DIR=/home/spapais/projects/rrg-swasland/datasets/nuscenes/
+    DATA_PKL_DIR=/home/spapais/datasets/nuscenes/
+fi
 
 # Container paths
 VOLUMES="--bind=$PROJ_DIR:/proj
@@ -54,6 +42,7 @@ CKPT_FILE=ckpts/$CFG_NAME.pth
 EVAL_OPT=jsonfile_prefix=$WRK_DIR
 
 # Command
+WANDB_MODE='offline'
 BASE_CMD="./tools/dist_test.sh $CFG_FILE $CKPT_FILE $NUM_GPUS --eval bbox --eval-options '$EVAL_OPT'"
 CONTAINER_CMD="apptainer exec --nv -c -e --pwd /proj/ \
 $VOLUMES \
@@ -61,11 +50,34 @@ $SING_IMG \
 $BASE_CMD
 "
 
-# Run
+# Start script
+echo "SLURM_JOB_ID=$SLURM_JOB_ID
+CFG_NAME=$CFG_NAME
+NUM_GPUS=$NUM_GPUS
+"
+
+# Extract dataset
+echo "Extracting data"
+SECONDS=0
+for file in $DATA_DIR/*.zip; do
+    duration=$SECONDS
+    echo "[$((duration/3600))h$((duration%3600/60))m]: Unzipping $file to $TMP_DATA_DIR"
+    unzip -qq $file -d $TMP_DATA_DIR
+done
+for file in $DATA_PKL_DIR/*.pkl; do
+    duration=$SECONDS
+    echo "[$((duration/3600))h$(((duration%3600)/60))m]: Copying $file to $TMP_DATA_DIR"
+    cp $file $TMP_DATA_DIR
+done
+echo "Done extracting data"
+
+# Run command
 # echo "Debug mode: sleep engaged" && sleep 5d
 module load StdEnv/2020
 module load apptainer
-echo "[$((duration/3600))h$(((duration%3600)/60))m]: Running eval"
+duration=$SECONDS
+echo "[$((duration/3600))h$(((duration%3600)/60))m]: Running command"
 echo "$CONTAINER_CMD"
 eval $CONTAINER_CMD
-echo "[$((duration/3600))h$(((duration%3600)/60))m]: Done eval"
+duration=$SECONDS
+echo "[$((duration/3600))h$(((duration%3600)/60))m]: Done"
