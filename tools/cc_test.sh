e17eb0db5003d4eb4ecde759eb4e883e29f1f561
Sandro Papais <papais.sandro@gmail.com>
2024-08-07 00:41:38 -0400

    update test time limit

--- tools/cc_test.sh
+++ tools/cc_test.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=rrg-swasland
 #SBATCH --ntasks=1                    # Run on n CPUs
 #SBATCH --mem=120gb                     # Job memory request
-#SBATCH --time=2-23:00:00               # Time limit hrs:min:sec
+#SBATCH --time=2:59:00               # Time limit hrs:min:sec
 #SBATCH --output=/home/spapais/output/streampetr_jdmp/%x-%j.log   # Standard output and error log
 #SBATCH --cpus-per-task=12
 #SBATCH --gres=gpu:t4:4           # gpu:t4:4 (graham) or gpu:a100:1 (narval)
