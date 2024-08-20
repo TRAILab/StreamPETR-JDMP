cd0899dfe2b2d7da9995bb1c377a9fafa9b42827
Sandro Papais <spapais@narval3.narval.calcul.quebec>
2024-08-20 18:29:49 -0400

    added hyperparameter for number of decoder layers

--- projects/mmdet3d_plugin/models/dense_heads/streampetr_head.py
+++ projects/mmdet3d_plugin/models/dense_heads/streampetr_head.py
@@ -186,7 +186,7 @@ class StreamPETRHead(AnchorFreeHead):
 
         self.act_cfg = transformer.get('act_cfg',
                                        dict(type='ReLU', inplace=True))
-        self.num_pred = 6
+        self.num_pred = detect_transformer['decoder']['num_layers']
         self.normedlinear = normedlinear
         super(StreamPETRHead, self).__init__(num_classes, in_channels, init_cfg = init_cfg)
 
