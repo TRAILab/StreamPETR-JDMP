d9da8981efaa731525489acbd4a82ce6321de6b4
Sandro Papais <papais.sandro@gmail.com>
2024-09-06 17:05:05 +0000

    fixed eval frequency

--- projects/configs/StreamPETR/jdmpvov_attforecast_noprop_graddetach_qembsep_6lay_attmem_bs8_2gpu.py
+++ projects/configs/StreamPETR/jdmpvov_attforecast_noprop_graddetach_qembsep_6lay_attmem_bs8_2gpu.py
@@ -280,7 +280,7 @@ lr_config = dict(
     min_lr_ratio=1e-3,
     )
 
-evaluation = dict(interval=num_iters_per_epoch*num_epochs, pipeline=test_pipeline)
+evaluation = dict(interval=2*num_iters_per_epoch, pipeline=test_pipeline)
 find_unused_parameters=False #### when use checkpoint, find_unused_parameters must be False
 checkpoint_config = dict(interval=num_iters_per_epoch, max_keep_ckpts=3)
 runner = dict(
