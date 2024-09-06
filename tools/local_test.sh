71ee136fccb73627a2c763af89c38cc732d2fb82
sandropapais <papais.sandro@gmail.com>
2024-09-06 19:38:01 -0400

    added multi test local support

--- tools/local_test.sh
+++ tools/local_test.sh
@@ -3,7 +3,7 @@
 # Parameters
 GPUS=0
 NUM_GPUS=1
-CONFIG_NAME=jdmp_mini_forecast_cv_bs16_1gpu
+CONFIG_NAME=jdmpvov_attforecast_prop_graddetach_qembsep_6lay_attmem_bs8_2gpu
 DOCKER_IMG=spapais/streampetr:latest
 
 # Host paths
@@ -15,8 +15,8 @@ OUTPUT_DIR=$PROJ_DIR/output
 # Container paths
 CONFIG_DIR=/proj/projects/configs/StreamPETR
 CONFIG_FILE=$CONFIG_DIR/$CONFIG_NAME.py
-MODEL_CKPT=/proj/output/$CONFIG_NAME/latest.pth
-WRK_DIR=/proj/output/$CONFIG_NAME
+MODEL_CKPT=/proj/output/$CONFIG_NAME/iter_19338.pth
+EVAL_OPT=jsonfile_prefix=/proj/output/$CONFIG_NAME/iter_19338
 
 VOLUMES="-v $PROJ_DIR/:/proj/
 -v $DATA_DIR/samples:/proj/data/nuscenes/samples
@@ -28,9 +28,19 @@ VOLUMES="-v $PROJ_DIR/:/proj/
 -v $DATA_DIR/sweeps:/proj/data/nuscenes/sweeps
 -v $OUTPUT_DIR:/proj/output/"
 
-DATE_TIME=$(date +"%Y%m%d_%H%M%S")
+# For single checkpoint
+BASE_CMD="tools/dist_test.sh $CONFIG_FILE $MODEL_CKPT $NUM_GPUS --eval bbox --eval-options '$EVAL_OPT'"
 
-BASE_CMD="tools/dist_test.sh $CONFIG_FILE $MODEL_CKPT $NUM_GPUS --eval bbox"
+# For multiple checkpoints
+# BASE_CMD="/bin/bash -c '"
+# MODEL_CKPTS=(output/$CONFIG_NAME/iter_*.pth)
+# echo "MODEL_CKPTS: ${MODEL_CKPTS[@]}"
+# for MODEL_CKPT in "${MODEL_CKPTS[@]}"
+# do
+#     EVAL_OPT=jsonfile_prefix=${MODEL_CKPTS%.*}
+#     BASE_CMD="${BASE_CMD} tools/dist_test.sh $CONFIG_FILE $MODEL_CKPT $NUM_GPUS --eval bbox --eval-options '$EVAL_OPT';"
+# done
+# BASE_CMD="${BASE_CMD}'"
 
 CONTAINER_CMD="docker run -d --ipc host --gpus $GPUS -w /proj/
 --env="WANDB_API_KEY=$WANDB_API_KEY"
