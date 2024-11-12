2b54f3d80e43cb50f12bf958ae42e8729592f0ab
Sandro Papais <spapais@narval1.narval.calcul.quebec>
2024-11-11 20:30:48 -0500

    fixed typo in log names

--- projects/configs/StreamPETR/jdmp_attforecast_prop_bs8_2gpu_freezedet.py
+++ projects/configs/StreamPETR/jdmp_attforecast_prop_bs8_2gpu_freezedet.py
@@ -10,7 +10,7 @@ log_config = dict(
             init_kwargs=dict(
                 entity='trailab',
                 project='JDMP',
-                name='jdmp_attforecast_noprop_bs8_2gpu_freezedet',),
+                name='jdmp_attforecast_prop_bs8_2gpu_freezedet',),
             interval=50)
     ])
 backbone_norm_cfg = dict(type='LN', requires_grad=True)
